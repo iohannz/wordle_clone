6440abdc53f9c72484e6308cad271a75368010a6
Daniel <perzhul.works@gmail.com  git config --global user.name Daniel>
2022-10-01 12:58:10 +0300

    letter state fix

--- src/store.ts
+++ src/store.ts
@@ -46,13 +46,6 @@ const matrixSlice = createSlice({
   initialState: initialState,
   reducers: {
     setLetter: (state, action) => {
-      console.log(
-        action.payload,
-        state.isShaking,
-        isWordValid(state),
-        state.currWord,
-        state.currentLetterIndex,
-      );
       if (action.payload === 'enter') {
         if (isWordValid(state)) {
           if (checkIfWon(state)) {
@@ -68,15 +61,27 @@ const matrixSlice = createSlice({
         } else {
           state.isShaking = true;
         }
-      } else if (action.payload === 'backspace' && state.currWord.length > 0) {
+      } else if (
+        action.payload === 'backspace' &&
+        state.currWord.length > -1 &&
+        state.currentLetterIndex >= 0
+      ) {
+        console.log(state.currWord.length, state.currWord);
         state.matrix[state.currentRowIndex][state.currentLetterIndex] = '';
+        state.currentLetterIndex -= 1;
         state.currWord = state.currWord.slice(0, -1);
-        if (state.currentLetterIndex > 0) {
-          state.currentLetterIndex -= 1;
-        }
+        // if (state.currentLetterIndex > 0) {
+        //   state.currentLetterIndex -= 1;
+        //   state.matrix[state.currentRowIndex][state.currentLetterIndex] = '';
+        // }
+        console.log(state.currWord.length, state.currWord);
       } else {
         if (action.payload.length === 1 && state.currWord.length < 5) {
-          state.matrix[state.currentRowIndex][state.currentLetterIndex] = action.payload;
+          console.log(action.payload);
+          if (state.currentLetterIndex < 0) {
+            state.currentLetterIndex = 0;
+          }
+          state.matrix[state.currentRowIndex][state.currWord.length] = action.payload;
           state.currWord += action.payload;
           if (state.currentLetterIndex < 4) {
             state.currentLetterIndex += 1;
