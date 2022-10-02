fea191f4881139d4843915f96f9f726a35576e15
Daniel <perzhul.works@gmail.com  git config --global user.name Daniel>
2022-10-02 22:07:52 +0300

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
   return (
-    <div
+    <header
       className="flex items-center justify-center
     border-b border-[rgb(58,58,60)] sm:h-[40px] md:h-[65px]
     px-[16px]">
-      <div className="normal-case font-helveticaBold font-bold text-[28px] md:text-[36px] text-text-main">
+      <span className=" cursor-default normal-case font-helveticaBold font-bold text-[28px] md:text-[36px] text-text-main">
         Wordle
-      </div>
-    </div>
+      </span>
+    </header>
   );
 };
 
